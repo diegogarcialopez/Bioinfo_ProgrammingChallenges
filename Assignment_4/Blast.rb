484afcb52ab34c30ec0aa49b956903ae89600712
diegogarcialopez <diego.garcia.lopez@alumnos.upm.es>
2021-12-21 14:53:44 -0500

    Done

--- Assignment_4/Blast.rb
+++ Assignment_4/Blast.rb
@@ -2,7 +2,7 @@ require 'bio'
 
 def make_db(file, type)                                                                   # Function to create databases
   db_name = file.split('.')[0].to_s                                                       # Assigning the name to the database as the file name without its extension
-  if type == Bio::Sequence::NA                                                            # Determining the database type. If the database is a nucleic or protein database
+  if type == Bio::Sequence::NA                                                            # Determining the database type. The database is either a nucleic or protein database
     db_type = 'nucl'
     system("makeblastdb -in '#{file}' -dbtype #{db_type} -out ./Databases/#{db_name}")
   elsif type == Bio::Sequence::AA
@@ -26,22 +26,22 @@ end
 
 def blast(query_id, query_sequence, blast1, blast2, sequences)                                                                       # Function to make the blasts
   puts "Searching for #{query_id} hits in Aabidopsis thaliana"
-  search_results = blast1.query(query_sequence)                                                                                      #  
+  search_results = blast1.query(query_sequence)                                                                                      # Blasting the sequence with the first blast method
   if not search_results.hits.empty?                                                                                                  # Checking if there are hits
     best_hit = search_results.hits[0]
     hit_id = best_hit.definition.match(/(\w+\.\w+)|/).to_s                                                                           # Getting the best hit id
-    hit_coverage = (best_hit.query_end.to_f - best_hit.query_start.to_f)/best_hit.query_len.to_f
+    hit_coverage = (best_hit.query_end.to_f - best_hit.query_start.to_f)/best_hit.query_len.to_f                                     # Calculating the query coverage
     if best_hit.evalue <= 10**-6 and hit_coverage >= 0.5                                                                             # Filtering for coverage and e-value
       puts "Searching for reciprocal hits for #{hit_id} in Schizosaccharomyces pombe"
-      reciprocal_search = blast2.query(sequences[hit_id])                                                                            # Making the reciprocal blast
+      reciprocal_search = blast2.query(sequences[hit_id])                                                                            # Making the reciprocal blast with the second blast method
       if not reciprocal_search.hits.empty?                                                                                           # Checking if there are hits
         reciprocal_hit = reciprocal_search.hits[0]
         reciprocal_hit_id = reciprocal_hit.definition.match(/(\w+\.\w+)|/).to_s                                                      # Getting the best hit id
-        reciprocal_hit_coverage = (reciprocal_hit.query_end.to_f - reciprocal_hit.query_start.to_f)/(reciprocal_hit.query_len.to_f)
+        reciprocal_hit_coverage = (reciprocal_hit.query_end.to_f - reciprocal_hit.query_start.to_f)/(reciprocal_hit.query_len.to_f)  # Calculating the query coverage
         if reciprocal_hit.evalue <= 10**-6 and reciprocal_hit_coverage >= 0.5                                                        # Filtering for coverage and e-value
           if query_id == reciprocal_hit_id                                                                                           # Checking that the first query id is the same as the obtained in the reciprocal blast
             puts "It is a recirpocal match!"
-            write_output_file(query_id, hit_id, best_hit.evalue, hit_coverage, reciprocal_hit.evalue, reciprocal_hit_coverage)                                                                                      # Writing the output file
+            write_output_file(query_id, hit_id, best_hit.evalue, hit_coverage, reciprocal_hit.evalue, reciprocal_hit_coverage)       # Writing the output file
           end
         end
       end
@@ -65,7 +65,7 @@ def blast_type(file_1, type_1, file_2, type_2)                          # Functi
     search1 = Bio::Blast.local('blastp', "./Databases/#{db1_name}")
     search2 = Bio::Blast.local('blastp', "./Databases/#{db2_name}")
   end
-  return [search1, search2]                                            # Returning the propoer blast and reciprocal blast according to the databases types
+  return [search1, search2]                                            # Returning the proper blast and reciprocal blast according to the databases types
 end
 
 def search_orthologues(file1, file2)                             # Function to search for the orthologues
@@ -78,15 +78,15 @@ def search_orthologues(file1, file2)                             # Function to s
     sequence1_hash[sequence.entry_id.to_s] = sequence.seq.to_s
   end
   system("mkdir Databases")                                      # Creating the folder for the databases
-  file1_type = make_db(file1, type1)                             # Creating the database for the first file
-  file2_type = make_db(file2, type2)                             # Creating the database for the second file
-  searches = blast_type(file1, file1_type, file2, file2_type)    # Determining the blast types
+  file1_type = make_db(file1, type1)                             # Creating the database for the first file and obtaining the database type
+  file2_type = make_db(file2, type2)                             # Creating the database for the second file and obtaining the database type
+  searches = blast_type(file1, file1_type, file2, file2_type)    # Obtaining the blast types
   search1 = searches[0]
   search2 = searches[1]
-  sequence2.each do |fasta_seq|
+  sequence2.each do |fasta_seq|                                  # Obtaining each sequence from the second file
     id = fasta_seq.entry_id.to_s
     sequence = fasta_seq.seq.to_s
-    blast(id, sequence, search1, search2, sequence1_hash)       # Performing the blast for each sequence in the second file
+    blast(id, sequence, search1, search2, sequence1_hash)        # Performing the blast for each sequence in the second file
   end
   puts "The results of the Blast are in the file called 'Blast_results.txt'"
 end
